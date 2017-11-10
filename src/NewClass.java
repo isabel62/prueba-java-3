5a9c2565faab6b107dd10d6b19a226b95721aa43
isabel62 <isabeliribarreng@hotmail.com>
2017-11-10 20:53:15 -0300

    renombrar las etiquetas

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,3 +1,9 @@
+
+import java.awt.Color;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +14,53 @@
  *
  * @author LAB08
  */
-public class NewClass {
+
+public class NewClass
+{
+ impoax.swt java systln new class
+ import java.awt.conexcion*;//   
     
 }
+public class ConexionBD
+        
+ private static Connection conex;
+
+ private static= String= ("TITULO" com.mysql.jdbc.Driver");"
+ private public=label mysql;
+private public=text jbdc;
+private public=driver;
+
+   private static String = "root";
+{
+   //PASO -declara CAMPOS Y VARIABLES
+  public = panel;
+   public label;
+   public text;
+   public boton;
+   public boton2;
+    
+            
+
+        }
+   private static final String PASS = "";
+   label for .conexcion;(scanner for "conexcion");
+
+   private static final String URL = "jdbc:mysql://localhost:3306/conexion";
+    
+  
+ label.setText("NOMBRE");
+   boton.setText("ENVIAR");
+   panel.setBackground(Color.red)
+
+
+  window ventana = new conexcion();//Creo un objeto tipo window
+        ventana.setVisible(true);
+        ventana.setBounds(0, 0,800, 800)
+        ventana.setTitle("mi primera aplicacion con ventana");
+        //la siguente sentecia es para finalizar la vetana
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+
+
+
+;
