e1a6dac88c6f4c0e56bb55c87d355f282c3769a7
isabel62 <isabeliribarreng@hotmail.com>
2017-11-10 21:30:09 -0300

    realiozar nueva clase

---
+++ src/NewClass1.java
@@ -0,0 +1,41 @@
+
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import new class
+public static void main (string[]args) 
+{
+systen.out.println(El programa es a ingrsado);                                                                                               
+/**
+ *
+ * @author LAB08
+ */
+public class NewClass1 
+
+  
+import java.awt.Color;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+  
+
+import class .interfaces
+  public ConexionBD()
+    {
+        conex = null;//Inicializar la variable 
+        
+        //Metodo para manejar excepciones
+        try
+        
+            Class.forName(DRIVER);
+            conex = DriverManager.getConnection(URL, USER, PASS);
+            if (conex != null) 
+        
+             System.out.println("Conexion Establecida");
+
+}
+}
